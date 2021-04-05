476bd1abf12677b23002bbdaa5d0ad86269daf12
Rabia Memon <rmemon25@gmail.com>
2021-04-05 16:11:19 +0500

    db connection

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express'); 
+const mongoose = require('mongoose');
+require('dotenv/config')
 
 const app = express(); 
+
+//Import Routes
+const postsRoute = require('./routes/posts')
+
+//Middleware
+app.use('/posts', postsRoute)
 
 //ROUTES 
 app.get('/', (req, res) => {
     res.send("We are on home");
 });
+
+//Connecting to DB 
+mongoose.connect(
+    process.env.DB_CONNECTION, 
+    { useNewUrlParser: true }, 
+    () => console.log('connected to DB')
+);
 
 app.listen(3000); 
 
