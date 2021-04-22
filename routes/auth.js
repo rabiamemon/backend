7be5f4df29c67fab25a27892cdb02f0cfe9d2764
Rabia Memon <rmemon25@gmail.com>
2021-04-22 14:40:47 +0500

    jwt token

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router(); 
 const User = require('../models/User'); 
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); 
 const {registerValidation, loginValidation} = require('../validation');
 
 
@@ -48,7 +49,11 @@ router.post('/login', async (req,res) => {
     const validPass = await bcrypt.compare(req.body.password, user.password)
     if (!validPass) return res.status(400).send('Invalid Password');
 
-    res.send('Logged in'); 
+    //Create and assign a token 
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
+
+    // res.send('Logged in'); 
 
 });
 
